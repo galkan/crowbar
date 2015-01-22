dd2b1ad8ed584167088a8bae412e2c5548248de5
Gökhan ALKAN <cigalkan@gmail.com>
2015-01-22 21:20:08 +0200

    Update threadpool.py

--- lib/core/threadpool.py
+++ lib/core/threadpool.py
@@ -8,36 +8,60 @@ except Exception, err:
   	from lib.core.exceptions import CrowbarExceptions
   	raise CrowbarExceptions(str(err))
 
-  	
+
 class Worker(Thread):
-    
+
     def __init__(self, tasks):
+
         Thread.__init__(self)
         self.tasks = tasks
         self.daemon = True
         self.start()
 
+
     def run(self):
+       
         while True:
-            func, args, kargs = self.tasks.get()
+                func, args, kargs = self.tasks.get(True, None)
 
-            try:
-                func(*args, **kargs)
-            except Exception, e:
-                 print e
+                if not func:
+                        break
 
-            self.tasks.task_done()
+                try:
+                        func(*args, **kargs)
+                except:
+                        pass
+
+		self.tasks.task_done()
 
 
 
-class ThreadPool():
-  
+class ThreadPool:
+
     def __init__(self, num_threads):
-        self.tasks = Queue(num_threads)
-        for _ in range(num_threads): Worker(self.tasks)
+
+        self.threads = []
+
+        self.num_threads = num_threads
+        self.tasks = Queue(self.num_threads)
+
+        for _ in range(self.num_threads): 
+                worker = Worker(self.tasks)
+                self.threads.append(worker)
+
 
     def add_task(self, func, *args, **kargs):
+
         self.tasks.put((func, args, kargs))
 
+
+
     def wait_completion(self):
+
         self.tasks.join()
+
+        for _ in range(self.num_threads):
+                self.add_task(None, None, None)
+
+        for t in self.threads:
+                t.join()
